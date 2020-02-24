b98332a18d7d7fc088b7214f5b0602d2e698b512
vbonczak <vincent.bonczak@gmail.com>
2020-02-24 22:35:45 +0100

    Refactorisation des ennemis mouvants

--- src/main/scala/enemies.scala
+++ src/main/scala/enemies.scala
@@ -2,8 +2,13 @@ import java.awt._
 import java.awt.geom.{AffineTransform, Point2D}
 import scala.math.{cos, sin, atan}
 
-abstract class LivingEnemy(life: Int) extends Enemy {
-	var lifePoints = life
+/**
+  * Living enemies all have 100 HP (health points). The manner
+  * they react to attacks is determined by the takeDamage procedure
+  * of the concrete enemies classes.
+  */
+abstract class LivingEnemy extends Enemy {
+	var lifePoints = 100
 
 	def isAlive(): Boolean = {
 		return lifePoints >= 0
@@ -17,18 +22,16 @@ abstract class LivingEnemy(life: Int) extends Enemy {
 	}
 }
 
-class SphereEnemy extends LivingEnemy(100) {
-	var gold:Int = 200
-	var speed:Double = 0.01
-
-	def getName(): String = {
-		return "High Dimensional Sphere"
-	}
-
-	def takeDamage(dmg: Int): Unit = {
-		lifePoints -= dmg
-	}
-
+/**
+  * The MovingEnemy class represents an enemy with a speed that can
+  * move across the board. It naturally extends LivingEnemy as in order
+  * to move, the enemy must be alive. 
+  *
+  * @param speed_coef The speed coefficient of the enemy. Each tick of the clock (60 times per second or so), the enemy will go further to this unit of distance.
+  */
+abstract class MovingEnemy(speed_coef: Double) extends LivingEnemy {
+	var speed = speed_coef
+	
 	def tick(running_for: Double, delta: Double) : Unit = {
 		var cp = GameStatus.map.checkpoints(targetedCheckpoint)
 		if (pos.distance(targetedCellPoint) <= speed) {
@@ -41,11 +44,23 @@ class SphereEnemy extends LivingEnemy(100) {
 			val r = scala.util.Random
 			targetedCellPoint = new CellPosition(cp.aX + r.nextFloat * (cp.bX - cp.aX), cp.aY + r.nextFloat * (cp.bY - cp.aY))
 		}
-		// println(f"Sphere $targetedCheckpoint, $targetedCellPoint")
+		// println(f"Sphere/Proto $targetedCheckpoint, $targetedCellPoint")
 
 		val theta = atan((targetedCellPoint.y - pos.y) / (targetedCellPoint.x - pos.x))
 		pos.move(speed * cos(theta), speed * sin(theta))
 	}
+}
+
+class SphereEnemy extends MovingEnemy(0.01) {
+	var gold:Int = 200
+	
+	def getName(): String = {
+		return "High Dimensional Sphere"
+	}
+
+	def takeDamage(dmg: Int): Unit = {
+		lifePoints -= dmg
+	}	
 
 	override def render(g: Graphics2D): Unit = {
 		val sPos = pos.toScreenPosition
@@ -55,9 +70,8 @@ class SphereEnemy extends LivingEnemy(100) {
 	}
 }
 
-class ProtoEnemy extends LivingEnemy(75) {
+class ProtoEnemy extends MovingEnemy(0.02) {
 	var gold:Int = 500
-	var speed:Double = 0.01
 
 	def getName(): String = {
 		return "Chess pawn"
@@ -67,29 +81,7 @@ class ProtoEnemy extends LivingEnemy(75) {
 		lifePoints -= dmg
 	}
 
-	def tick(running_for: Double, delta: Double) : Unit = {
-		var cp = GameStatus.map.checkpoints(targetedCheckpoint)
-		if (pos.distance(targetedCellPoint) <= speed) {
-			targetedCheckpoint = cp.next
-			if (targetedCheckpoint == -1) {
-				valid = false
-				return
-			}
-			cp = GameStatus.map.checkpoints(targetedCheckpoint)
-			val r = scala.util.Random
-			targetedCellPoint = new CellPosition(cp.aX + r.nextFloat * (cp.bX - cp.aX), cp.aY + r.nextFloat * (cp.bY - cp.aY))
-		}
-		// println(f"Proto $targetedCheckpoint, $targetedCellPoint")
-
-		val theta = atan((targetedCellPoint.y - pos.y) / (targetedCellPoint.x - pos.x))
-		pos.move(speed * cos(theta), speed * sin(theta))
-	}
-
 	override def render(g: Graphics2D): Unit = {
-		/*val sPos = pos.toScreenPosition
-		g.setColor(new Color(80, 255, 255, 0))
-		g.fillRect(sPos.x-20, sPos.y-20, 40, 40)
-		drawHealthBar(g, sPos + new ScreenPosition(0, -60))*/
 		val s:Image = SpriteLoader.fromResource("pion.png")
 		val sPos = pos.toScreenPosition
 		g.drawImage(s, new AffineTransform(0.2, 0, 0, 0.2, sPos.x-40, sPos.y-40), null)
