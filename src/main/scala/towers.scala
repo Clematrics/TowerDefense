609483312f9a37418827ba6ef5024187a4f2beaf
vbonczak <vincent.bonczak@gmail.com>
2020-02-24 20:14:30 +0100

    Deuxième type de tour

--- src/main/scala/towers.scala
+++ src/main/scala/towers.scala
@@ -18,3 +18,21 @@ class ProtoTower extends Tower {
 		g.drawImage(s, new AffineTransform(0.1, 0, 0, 0.1, sPos.x-40, sPos.y-40), null)	
 	}
 }
+
+class ArmedTower extends Tower {
+	var pos: CellPosition = new CellPosition(15, 10)
+	var radius:Double = 3
+	def getName(): String = {
+		return "Armed Tower"
+	}
+
+	def tick(running_for: Double, delta: Double) : Unit = {
+		
+	}
+
+	override def render(g: Graphics2D): Unit = {
+		val s:Image = SpriteLoader.fromResource("armedtour.png")
+		val sPos = pos.toScreenPosition
+		g.drawImage(s, new AffineTransform(0.1, 0, 0, 0.1, sPos.x-40, sPos.y-40), null)	
+	}
+}
