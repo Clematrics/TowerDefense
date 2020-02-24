4f394e76bce915e8164a9df429baf35377e0d4aa
vbonczak <vincent.bonczak@gmail.com>
2020-02-24 18:56:27 +0100

    Correction des imports dans towers.scala

--- src/main/scala/towers.scala
+++ src/main/scala/towers.scala
@@ -1,3 +1,6 @@
+import java.awt._
+import java.awt.geom.{AffineTransform, Point2D}
+
 class ProtoTower extends Tower {
 	var pos: CellPosition = new CellPosition(15, 10)
 
@@ -9,7 +12,7 @@ class ProtoTower extends Tower {
 		
 	}
 
-	def render(g: Graphics2D): Unit = {
+	override def render(g: Graphics2D): Unit = {
 		val s:Image = SpriteLoader.fromResource("tour.png")
 		val sPos = pos.toScreenPosition
 		g.drawImage(s, new AffineTransform(0.1, 0, 0, 0.1, sPos.x-40, sPos.y-40), null)	
