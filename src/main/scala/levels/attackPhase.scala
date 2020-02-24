decd0c7e45af1649d889ee335f2ba1096dc26a28
vbonczak <vincent.bonczak@gmail.com>
2020-02-24 19:15:38 +0100

    Nettoyage de code divers

--- src/main/scala/levels/attackPhase.scala
+++ src/main/scala/levels/attackPhase.scala
@@ -49,7 +49,7 @@ class AttackPhase extends Level { outer =>
 		g.drawImage(GameStatus.map.mapImg, new AffineTransform(24, 0, 0, 24, 0, 0), null)
 		for(cp <- GameStatus.map.checkpoints) {
 			val stroke = new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, Array(10, 5), 0)
-			g.setStroke(stroke);
+			g.setStroke(stroke)
 			g.setColor(new Color(255, 0, 255, 255))
 			g.drawLine(cp.aX * 24, cp.aY * 24, cp.bX * 24, cp.bY * 24)
 		}
