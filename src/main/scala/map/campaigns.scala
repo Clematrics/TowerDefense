8a31fd1578db6d711d7cb8a694a9772d1f9e838a
Clematrics <clematrics.studio@gmail.com>
2020-02-29 11:46:43 +0100

    Rétablissement des campagnes

--- src/main/scala/map/campaigns.scala
+++ src/main/scala/map/campaigns.scala
@@ -7,7 +7,35 @@ object Campaigns {
 		new Campaign(
 			"The valley of prime numbers",
 			List(MapLoader.loadMap("map"), MapLoader.loadMap("mapSimple"))
-		)
+		),
+		new Campaign(
+			"The sea of differential equations",
+			List(MapLoader.loadMap("map"))
+		),
+		new Campaign(
+			"Number theory's battlefield",
+			List(MapLoader.loadMap("map"), MapLoader.loadMap("map"))
+		),
+		new Campaign(
+			"The shitfuckery of Logics and Semantics",
+			List(MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"))
+		),
+		new Campaign(
+			"Into the maze of quantum theory",
+			List(MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"))
+		),
+		new Campaign(
+			"The curse of hypercomputation",
+			List(MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"))
+		),
+		new Campaign(
+			"The story of Levi-Civita in Lorentzian manifolds",
+			List(MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"))
+		),
+		new Campaign(
+			"The castle of complexity classes",
+			List(MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"), MapLoader.loadMap("map"))
+		),
 	)
 
 	def previousCampaign(): Campaign = {
