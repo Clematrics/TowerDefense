410fb9d651587aac3795205fd9e8f8b94c5b623b
vbonczak <vincent.bonczak@gmail.com>
2020-03-27 22:07:06 +0100

    Prototype d'un ennemi volant (dragon)

---
+++ src/main/scala/game/enemies/flyingEnemy.scala
@@ -0,0 +1,34 @@
+import engine.core.Renderer
+import engine.helpers.{CellPoint, ScreenPoint}
+import engine.loaders.SpriteLoader
+import java.awt.geom.AffineTransform
+import java.awt._
+
+/**
+  * The enemy for the demonstration of an animation.
+  *
+  */
+class FlyingEnemy extends MovingEnemy with LivingEnemy {
+	speed = 0.1
+	var gold:Int = 250
+
+	def getName(): String = {
+		return "Dragon"
+	}
+
+	def takeDamage(dmg: Int): Unit = {
+		setLifePoints(lifePoints - dmg)
+	}
+
+	def getGold(): Int = {
+		return gold
+	}
+
+	def render(time: Double, delta: Double): Unit = {
+		val s:Image = SpriteLoader.fromResource("anim/dragon" + (time/100 % 11).toInt +".png")
+		val sPos = pos.toScreenPosition
+		//TODO : manage directions?
+		Renderer.groundEntities.drawImage(s, new AffineTransform(-0.4, 0, 0, 0.4, sPos.x + 35, sPos.y  - 15), null)
+		drawHealthBar(sPos + new ScreenPoint(0, -30))
+	}
+}
