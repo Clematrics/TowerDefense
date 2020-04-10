7938ec0091f6fc75d0387a72167c88310e668dfd
vbonczak <vincent.bonczak@gmail.com>
2020-04-10 11:16:56 +0200

    Problème dans le comportement de ThunderTower résolu

--- src/main/scala/game/towers/thunderTower.scala
+++ src/main/scala/game/towers/thunderTower.scala
@@ -19,29 +19,33 @@ class ThunderTower extends RadiusTower(5, 10000, 1) {
 		return "Thunder Tower"
 	}
 
-	def tick(time: Double, delta: Double) : Unit = {
+	def tick(time: Double, delta: Double): Unit = {
 		if (lastShot + reload < time) {
-			var enemiesNear : Array[MovingEnemy] = Game.getEnemiesAround(pos, radius).map(e => e.asInstanceOf[MovingEnemy])
+			var enemiesNear: Array[MovingEnemy] = Game.getEnemiesAround(pos, radius).map(e => e.asInstanceOf[MovingEnemy])
 
-			var speeds : mutable.HashMap[MovingEnemy, Double] = mutable.HashMap.empty[MovingEnemy, Double]
+			var speeds: mutable.HashMap[MovingEnemy, Double] = mutable.HashMap.empty[MovingEnemy, Double]
+
+			Game.entities += new ShockWave(time, this, Color.CYAN)
+			lastShot = time
 
 			for (e <- enemiesNear) {
 				speeds += (e -> e.speed)
 				e.speed = 0
-				Game.entities += new ShockWave(time, this, Color.CYAN)
-				lastShot = time
 			}
 
 			new Delay(5000, () => {
 				for (e <- enemiesNear) {
-					e.speed = speeds(e)
+					if (e != null)
+						e.speed = speeds(e)
 				}
-			}) { run = true }
+			}) {
+				run = true
+			}
 		}
 	}
 
 	def render(time: Double, delta: Double): Unit = {
-		val s:Image = SpriteLoader.fromResource("thundertour.png")
+		val s: Image = SpriteLoader.fromResource("thundertour.png")
 		val sPos = pos.toScreenPosition
 		Renderer.groundEntities.drawImage(s, new AffineTransform(1.3, 0, 0, 1.3, sPos.x - 15, sPos.y - 8), null)
 	}
